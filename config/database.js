3a68c0dbc81ed1f47f296941930522e090082387
Djotiguy <97668821+Djotiguy@users.noreply.github.com>
2022-12-05 13:59:09 +0100

    initialistation du projet

---
+++ config/database.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+//connexion à la base de données
+mongoose.connect('mongodb://localhost:27017/db');
+
+mongoose.connection.on("error", () => {
+  console.log("Erreur lors de la connexion à la base de données");
+})
+
+mongoose.connection.on("open", () => {
+  console.log("Connexion à la base de données établie");
+})
+
+
+let TeaCupSchema = mongoose.Schema({
+  title: String,
+  description: String,
+  category: String,
+  date: Date,
+  images: [{
+    src: String,
+    alt: String
+  }]
+})
+
+// let UserSchema = mongoose.Schema({
+//  email: String,
+//  password: String,
+//  pseudo: String,
+// })
+
+let Tea = mongoose.model("tea", TeaCupSchema)
+
+// let User = mongoose.model("User", UserSchema)
+
+export default Tea;
